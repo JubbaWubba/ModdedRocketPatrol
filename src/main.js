69f20ae033f57106a445e2414d184b7b45585586
JubbaWubba <gabrielmaghanoyr@gmail.com>
2022-04-17 14:40:51 -0700

    comments

--- src/main.js
+++ src/main.js
@@ -12,3 +12,12 @@ let borderPadding = borderUISize / 3;
 
 // reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT, KeyUp, keyDown, keyA, keyD, keyW, keyS;
+
+// Gabriel Rivera, Modded Rocket Patrol, 4/17/22, took around 15~ hours overall, 
+// New Starship -(20) points
+// New Weapon - (20) points
+// New Timing/Scoring that adds time to clock (ONLY IN SINGLEPLAYER) -(20) points
+// simultaneous two-player mode - (30) points
+// Display Time in seconds on screen - (10) points
+// Control Rocket after fired - (5) points
+// Copyright Free background music - (5) points
