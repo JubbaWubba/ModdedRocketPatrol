857e1346a747d231d8fcad62172b66acda0602fe
JubbaWubba <gabrielmaghanoyr@gmail.com>
2022-06-01 11:11:08 -0700

    stuff

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -92,7 +92,7 @@ class Play extends Phaser.Scene {
         this.GameOver = false;
         // 60-second play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
+        this.clock = this.time.delayedCall(100, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or ← to Menu', scoreConfig).setOrigin(0.5);
             this.GameOver = true;
